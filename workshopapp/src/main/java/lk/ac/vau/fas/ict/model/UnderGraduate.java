da4a9236fdca7b5df3bb3f65c152276aed364d95
shanusen27 <shashinianuththara20@gmail.com>
2025-05-05 09:21:38 +0530

    updating models

--- workshopapp/src/main/java/lk/ac/vau/fas/ict/model/UnderGraduate.java
+++ workshopapp/src/main/java/lk/ac/vau/fas/ict/model/UnderGraduate.java
@@ -1,5 +1,26 @@
 package lk.ac.vau.fas.ict.model;
 
-public class UnderGraduate {
+import jakarta.persistence.*;
 
+import java.util.List;
+
+@Entity
+public class UnderGraduate {
+    @Id
+    private String id;
+
+    private String name;
+    private String email;
+    private String gender;
+    private String phone;
+    private String degree;
+    private String university;
+
+    @ManyToMany
+    @JoinTable(
+        name = "under_graduate_sessions",
+        joinColumns = @JoinColumn(name = "under_graduates_id"),
+        inverseJoinColumns = @JoinColumn(name = "sessions_id")
+    )
+    private List<Session> sessions;
 }
--- workshopapp/src/main/java/lk/ac/vau/fas/ict/model/Workshops.java
+++ workshopapp/src/main/java/lk/ac/vau/fas/ict/model/Workshops.java
@@ -1,5 +1,24 @@
 package lk.ac.vau.fas.ict.model;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
 public class Workshops {
+    @Id
+    private int id;
+
+    private String description;
+    private String name;
+
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    @OneToMany(mappedBy = "workshops")
+    private List<Session> sessions;
 
 }
