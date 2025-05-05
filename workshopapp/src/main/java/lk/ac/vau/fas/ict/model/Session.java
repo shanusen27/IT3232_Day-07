17ae4f0a154d5a80bde23108e19391e228b15832
shanusen27 <shashinianuththara20@gmail.com>
2025-05-05 09:20:38 +0530

    updating models

--- workshopapp/src/main/java/lk/ac/vau/fas/ict/model/Session.java
+++ workshopapp/src/main/java/lk/ac/vau/fas/ict/model/Session.java
@@ -1,5 +1,39 @@
 package lk.ac.vau.fas.ict.model;
 
-public class Session {
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+
+public class Session {
+    @Id
+    private String id;
+    
+    private String name;
+    private String description;
+    private LocalDate date;
+    private LocalTime time;
+
+    @ManyToOne
+    @JoinColumn(name = "workshop_id")
+    private Workshops workshops;
+
+    @ManyToMany(mappedBy = "sessions")
+    private List<Presenter> presenters;
+
+    @ManyToMany(mappedBy = "sessions")
+    private List<PostGraduate> postGraduates;
+
+    @ManyToMany(mappedBy = "sessions")
+    private List<UnderGraduate> underGraduates;
 }
+
+
+
