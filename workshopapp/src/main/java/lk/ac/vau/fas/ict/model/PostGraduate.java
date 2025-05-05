5c5df0bc3763f46908d389c62c5c5787855bbcbb
shanusen27 <shashinianuththara20@gmail.com>
2025-05-05 09:19:03 +0530

    updating models

--- workshopapp/src/main/java/lk/ac/vau/fas/ict/model/PostGraduate.java
+++ workshopapp/src/main/java/lk/ac/vau/fas/ict/model/PostGraduate.java
@@ -1,5 +1,28 @@
 package lk.ac.vau.fas.ict.model;
 
-public class PostGraduate {
+import jakarta.persistence.*;
 
+import java.util.List;
+
+@Entity
+
+public class PostGraduate {
+    @Id
+    private String id;
+
+    private String name;
+    private String email;
+    private String gender;
+    private String phone;
+    private String institute;
+    private String researchInterest;
+    private String secondDegree;
+
+    @ManyToMany
+    @JoinTable(
+        name = "post_graduate_sessions",
+        joinColumns = @JoinColumn(name = "post_graduates_id"),
+        inverseJoinColumns = @JoinColumn(name = "sessions_id")
+    )
+    private List<Session> sessions;
 }
--- workshopapp/src/main/java/lk/ac/vau/fas/ict/model/Presenter.java
+++ workshopapp/src/main/java/lk/ac/vau/fas/ict/model/Presenter.java
@@ -1,5 +1,31 @@
 package lk.ac.vau.fas.ict.model;
 
-public class Presenter {
+import java.util.List;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+
+@Entity
+public class Presenter {
+    @Id
+    private String id;
+
+    private String name;
+    private String email;
+    private String gender;
+    private String phone;
+    private String affiliation;
+    private String country;
+    private String jobTitle;
+
+    @ManyToMany
+     @JoinTable(
+        name = "present_session",
+        joinColumns = @JoinColumn(name = "presenter_id"),
+        inverseJoinColumns = @JoinColumn(name = "session_id")
+    )
+    private List<Session> sessions;
 }
